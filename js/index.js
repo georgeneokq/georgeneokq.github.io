ff7a88c8f8a3fc46f6c1473097fbee516bbeee13
George <george.neokq@gmail.com>
2022-09-29 01:18:34 +0800

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,25 @@
+﻿function saveName() {
+  let input = document.getElementById("input-name");
+  let name = input.value;
+  localStorage.name = name;
+  window.location.reload();
+}
+
+function changeColor() {
+  let input = document.getElementById("input-color");
+  let color = input.value;
+  document.body.style.background = color;
+}
+
+let counter = 1;
+
+window.setInterval(() => {
+  const img = document.querySelector("#kitty-image");
+  
+  if(counter % 2 === 1) {
+    img.style.animation = "spin-reverse 5s linear infinite";
+  } else {
+    img.style.animation = "spin 5s linear infinite";
+  }
+  counter++;
+}, 5000);
